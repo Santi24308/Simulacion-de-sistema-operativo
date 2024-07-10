5493e4d27d6ac8e9c5a9076ca08413168e09a4f5
Aylen Dubini <132672521+AyluDubini@users.noreply.github.com>
2024-07-10 13:21:48 -0300

    Agrego logger para test

--- entradasalida/src/io.c
+++ entradasalida/src/io.c
@@ -526,11 +526,21 @@ void ejecutar_fs_truncate(){
 	int tamanio_archivo = config_get_int_value(archivo_buscado->metadata, "TAMANIO_ARCHIVO");
 
 
+	log_info(logger_io , "Cantidad de bloques del archivo: %s antes de modificacion: %d" ,archivo_buscado->nombre_archivo, tamanio_archivo );
 
-	if (tamanio_solicitado < tamanio_archivo)
+
+	if (tamanio_solicitado < tamanio_archivo){
+
 		reducir_tamanio(archivo_buscado, tamanio_solicitado);
-	else	
+		int tamanio_archivo_actualizado = config_get_int_value(archivo_buscado->metadata, "TAMANIO_ARCHIVO");
+		log_info(logger_io , "Cantidad de bloques del archivo : %s  despues de la reduccion : %d " , archivo_buscado->nombre_archivo, tamanio_archivo_actualizado);
+	}
+	else {
+
 		ampliar_tamanio(archivo_buscado, tamanio_solicitado);
+		int tamanio_archivo_actualizado = config_get_int_value(archivo_buscado->metadata, "TAMANIO_ARCHIVO");
+		log_info(logger_io , "Cantidad de bloques del archivo : %s  despues de la ampliacion : %d " , archivo_buscado->nombre_archivo, tamanio_archivo_actualizado);
+	}
 
 }
 
